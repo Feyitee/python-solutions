2110ee07fae19c57b881ff524eff4ae24b3e483f
Feyitee <anjuwontobi99@gmail.com>
2026-09-08 12:21:16 +0100

    solution-4&5

--- solutions/solution.py
+++ solutions/solution.py
@@ -39,9 +39,49 @@ print(remove_outliers2(scores))
 
 
 # Problem 3 (The Sub-list Slicer)
-
 items = ["A", "B", "C", "D", "E", "F", "G"]
 def sub_listslicer(data):
     return data[2:5]
 
 print(sub_listslicer(items))
+
+# Problem 4 of 100: The Evens CombinerGoal: Take two separate lists of numbers, 
+# combine them into one list, but only keep the even numbers.
+# Instructions:Write a function called combine_evens(list1, list2)that takes two lists of integers. 
+# Combine them together and return a new list containing only the even numbers.Example
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+def combine_goals(data1, data2 ):
+    combine = data1 + data2
+    result = []
+    for num in combine:
+        if num % 2 == 0:
+            result.append(num)
+    return result
+
+print(combine_goals(a, b))
+
+# Problem 5 of 100: The Short Word FilterGoal: Filter a list of strings using a single line of code
+# (List Comprehension).Instructions:Write a function called filter_short_words(words) that takes a list of strings.
+# It should return a new list containing only the words that have 4 characters or more.Challenge requirement: 
+# Write the filtering logic inside the function using a list comprehension instead of a traditional multi-line for loop.
+
+
+# The traditional For_Loop style
+my_words = ["cat", "dog", "apple", "bird", "go", "python"]
+def filter_goal(data):
+    result = []
+    for char in my_words:
+        if len(char) >= 4:
+            result.append(char)
+    return result
+# print(filter_goal(my_words))
+
+#List comprehension
+
+def filter_goall(data):
+    return [char for char in data if len(char) >= 4]
+
+print(filter_goall(my_words))
