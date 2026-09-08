dc509dd355ad0f1296aa18f35c01ece378bd7176
Feyitee <anjuwontobi99@gmail.com>
2026-09-08 11:57:26 +0100

    solution-3

--- solutions/solution.py
+++ solutions/solution.py
@@ -14,17 +14,17 @@ print(swap_extremes(my_list))
 # Problem 2 of 100: The Outlier RemoverGoal: Clean up a list by removing the highest and lowest values, regardless of where they sit in the list.Instructions:Write a function called remove_outliers(data) that takes a list of numbers. It needs to find the absolute minimum value and the absolute maximum value, 
 # remove them both from the list, and return the cleaned list.(Assume all numbers in the list are unique for now).
 scores = [12, 45, 2, 89, 34]
-# def remove_outliers(data):
-#     highest = data[0]
-#     lowest = data[0]
-#     for num in data:
-#         if num > highest:
-#             highest = num
-#         if num < lowest:
-#             lowest = num
-#     data.remove(highest)
-#     data.remove(lowest)
-#     return data
+def remove_outliers(data):
+    highest = data[0]
+    lowest = data[0]
+    for num in data:
+        if num > highest:
+            highest = num
+        if num < lowest:
+            lowest = num
+    data.remove(highest)
+    data.remove(lowest)
+    return data
 
 # fastest_solution
 def remove_outliers2(data):
@@ -38,3 +38,10 @@ def remove_outliers2(data):
 print(remove_outliers2(scores))
 
 
+# Problem 3 (The Sub-list Slicer)
+
+items = ["A", "B", "C", "D", "E", "F", "G"]
+def sub_listslicer(data):
+    return data[2:5]
+
+print(sub_listslicer(items))
