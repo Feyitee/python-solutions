2c35068026d7f3669d6fa5adf0cebfdf555a0022
Feyitee <anjuwontobi99@gmail.com>
2026-09-23 12:27:10 +0100

    classes

--- solutions/solution7.py
+++ solutions/solution7.py
@@ -9,9 +9,30 @@ def filter_prices(prices, cutoff):
     return result
 
 
-
 response =  {
     "model": "gpt",
     "score" : 0.92
 }
 response["score"]
+
+numbers = [ 3, 5, 7, 5, 9]
+seen = set()
+
+for value in numbers:
+    if value in seen:
+        print (value)
+    seen.add(value)
+
+
+
+# Class blueprint - Things created from class
+
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+        
+michael = Student("Michael", 25)
+sarah = Student("Sarah", 15)
